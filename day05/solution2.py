fe2590dfa440bf5ed7dee133cf4ee8763103a9ac
rhansenne <rami.hansenne@digipolis.gent>
2024-12-05 07:21:32 +0100

    day05

--- day05/solution2.py
+++ day05/solution2.py
@@ -5,18 +5,17 @@ for l in open('input.txt', 'r').readlines():
         rules.append(l.strip().split('|'))
     elif ',' in l:        
         pages=l.strip().split(',')
-        ok=True
-        for r in rules:
-            if r[0] in pages and r[1] in pages and pages.index(r[1])<pages.index(r[0]):
-                ok=False
+        resorted=False
+        while True:
+            sorted=True
+            for r in rules:
+                if r[0] in pages and r[1] in pages and pages.index(r[1])<pages.index(r[0]):
+                    i0, i1 = pages.index(r[0]), pages.index(r[1])
+                    pages[i1], pages[i0] = pages[i0], pages[i1]
+                    sorted=False
+                    resorted=True
+            if sorted:
                 break
-        if not ok:
-            while not ok:
-                ok=True
-                for r in rules:
-                    if r[0] in pages and r[1] in pages and pages.index(r[1])<pages.index(r[0]):
-                        ok=False
-                        i0, i1 = pages.index(r[0]), pages.index(r[1])
-                        pages[i1], pages[i0] = pages[i0], pages[i1]
+        if resorted:
             sum+=int(pages[int((len(pages)-1)/2)])
 print(sum)
