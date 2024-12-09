89f7b9475857d02ca0b08e7e54bf3903e9565017
rhansenne <rami.hansenne@digipolis.gent>
2024-12-09 08:12:16 +0100

    day09

---
+++ day09/solution2.py
@@ -0,0 +1,45 @@
+disk=[]
+isFile=True
+id=0
+for dm in open('input.txt', 'r').readline():  #explode disk map
+    for block in range(int(dm)): 
+        if isFile:
+            disk+=[id]
+        else: 
+            disk+=[-1]
+    isFile=not isFile
+    if isFile:
+        id+=1
+
+firstSpace=0
+lastBlock=len(disk)-1        
+while firstSpace < lastBlock: #move blocks
+    moved=False    
+    while firstSpace < lastBlock and not moved:
+        while disk[firstSpace]>=0:
+            firstSpace+=1
+        lenSpace=1
+        if firstSpace > lastBlock:
+            break
+        while disk[firstSpace+lenSpace]<0:
+            lenSpace+=1
+        while disk[lastBlock]<0:
+            lastBlock-=1
+        lenBlock=1
+        while disk[lastBlock-lenBlock]==disk[lastBlock]:
+            lenBlock+=1
+        if lenBlock<=lenSpace:
+            for i in range(lenBlock):
+                disk[firstSpace+i], disk[lastBlock-i] = disk[lastBlock-i], disk[firstSpace+i]
+            moved=True
+        else:
+            firstSpace+=lenSpace            
+    firstSpace=0
+    lastBlock-=lenBlock
+
+checksum=0
+for i in range(0,len(disk)): #calculate checksum
+    if disk[i]==-1:
+        continue
+    checksum+=i*disk[i]
+print(checksum)
