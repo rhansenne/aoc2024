b4a4829f9436597ef4e1f38afbfa0778039d26d9
rhansenne <rami.hansenne@digipolis.gent>
2024-12-13 08:27:50 +0100

    day13

---
+++ day13/solution1.py
@@ -0,0 +1,18 @@
+import re
+from sympy.solvers import solve
+from sympy.abc import a,b
+
+tokens=0
+for l in open('input.txt', 'r').readlines():
+    co = re.findall(r"([\d]+)", l)
+    if 'A' in l:
+        ax,ay=int(co[0]),int(co[1])
+    elif 'B' in l:
+        bx,by=int(co[0]),int(co[1])
+    elif 'P' in l:
+        px,py=int(co[0]),int(co[1])
+    else:
+        sol = solve([a*ax+b*bx-px, a*ay+b*by-py], [a, b])
+        if sol[a].is_integer and sol[b].is_integer:
+            tokens+=3*sol[a]+sol[b]
+print(tokens)
